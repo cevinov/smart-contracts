f17f26502e042b24f2dce399e42c2264609bdc8b
cevinov <admin@vino0333.my.id>
2024-02-16 21:45:07 +0700

    connect to blockchain with BSC RPC endpoint

--- pancakeswapV2/client/ABIList.js
+++ pancakeswapV2/client/ABIList.js
@@ -138,7 +138,7 @@
 // console.log(ABIToken[0].status);
 
 // Select the required functions from the smart contract code
-const decimalValue = "function decimals() external pure returns (uint8)";
+const erc20Decimal = "function decimals() external pure returns (uint8)";
 
 // This function resides in the address factory source code, to return a list of smart contracts that interact with token0 (addressFrom) and token1 (addressTo).
 // https://bscscan.com/address/0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73#code
@@ -162,19 +162,19 @@ const allPairs = [
 // Function to get token price details, with ABI router
 // https://bscscan.com/address/0x10ed43c718714eb63d5aa57b78b54704e256024e#code
 const routerABI = [
-  "function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts)",
+  "function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts)",
   "function getAmountsOut(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts)",
   "function swapTokensForExactTokens(uint amountOut,uint amountInMax,address[] calldata path,address to,uint deadline) external returns (uint[] memory amounts)",
 ];
 
 module.exports = {
-  decimalValue,
+  erc20Decimal,
   factoryABI,
   pairABI,
   allPairs,
+  routerABI,
 };
-
-console.log(decimalValue);
+// console.log(erc20Decimal);
 
 /* NB: 
 1. The number of decimal places used to represent the token. For example, if the token has 18 decimal places, one token is equal to 1000000000000000000 (10^18) units.
--- pancakeswapV2/client/getPrices.js
+++ pancakeswapV2/client/getPrices.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat"); // Need this to remove ReferenceError: ethers is not defined
+
 const {
   addressFactory,
   addressRouter,
@@ -5,4 +7,36 @@ const {
   addressTo,
 } = require("./addressList");
 
-console.log(addressFrom, addressTo);
+const {
+  erc20Decimal,
+  factoryABI,
+  pairABI,
+  allPairs,
+  routerABI,
+} = require("./ABIList");
+
+// console.log(addressFrom, addressTo, allPairs);
+/*
+
+
+*/
+// What is RPC endpoint?: https://101blockchains.com/rpc-node/
+// RPC endpoint used to send commands to blockchain nodes and receive responses
+// https://docs.bnbchain.org/docs/rpc/
+url = "https://bsc-dataseed.bnbchain.org";
+
+// The next thing is connected to the blockchain with the provider (BSC RPC endpoint) using ethers library
+const provider = new ethers.providers.JsonRpcProvider(url);
+// console.log(provider);
+
+// Connect to factory address using provider
+const contractFactory = new ethers.Contract(addressFactory, factoryABI);
+// console.log(contractFactory);
+
+// Connect to router address
+const contractRouter = new ethers.Contract(addressRouter, routerABI);
+// console.log(contractRouter);
+/*
+
+
+*/
