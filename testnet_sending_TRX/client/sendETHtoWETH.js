f06c26cdbdee3c662dbc62306d730672ca36df3b
cevinov <admin@vino0333.my.id>
2024-02-24 00:47:00 +0700

    update using infura

--- testnet_sending_TRX/client/sendETHtoWETH.js
+++ testnet_sending_TRX/client/sendETHtoWETH.js
@@ -1,16 +1,15 @@
 const { ethers } = require("ethers");
 
 // Set up a provider to connect to the testnet network (sepolia)
-const rpcUrl =
-  "https://eth-sepolia.g.alchemy.com/v2/wQ6iV99uGPGeKk7H7nKlZLbNLcz3SCu6";
+const rpcUrl = "https://goerli.infura.io/v3/5ab76f78c3f34a36b1c7e23bede5d927";
 const providerTestnet = new ethers.providers.JsonRpcProvider(rpcUrl);
 
-const amountShipped = "0.1"; // Total amount of ETH to be sent
+const amountShipped = "0.003"; // Total amount of ETH to be sent
 
 // Create signer with privateKey from wallet to sign transaction on blockchain
 const walletAddress = "0x28D40a1c05Ace3AE7Ce42e289a14cb8268ACe2e4";
 const privateKey =
-  "52d5291354b9bdb23866e3c5c619ab9bd19b50c15272496296d4252a3eb63761";
+  "0x52d5291354b9bdb23866e3c5c619ab9bd19b50c15272496296d4252a3eb63761";
 const walletSigner = new ethers.Wallet(privateKey, providerTestnet);
 // console.log(walletSigner);
 /*
@@ -18,13 +17,13 @@ const walletSigner = new ethers.Wallet(privateKey, providerTestnet);
 */
 const exchangeETH = async () => {
   const gasPrice = await providerTestnet.getGasPrice(); // Check recommendation for gasPrice (Priority)
-  const nonce = 5; // an incrementing value indexed per owner,token,and spender for each signature (So change it every time we send a transaction)
+  const nonce = 2; // an incrementing value indexed per owner,token,and spender for each signature (So change it every time we send a transaction)
   // https://docs.uniswap.org/contracts/permit2/reference/allowance-transfer
 
   // Build transactions that are represented as objects
   const trxBuild = {
     from: walletAddress,
-    to: "0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa", // Import this token address into MetaMask wallet (sepolia)
+    to: "0x0B1ba0af832d7C05fD64161E0Db78E85978E8082", // Import this token address into MetaMask wallet (sepolia)
     value: ethers.utils.parseEther(amountShipped),
     nonce: nonce,
     gasLimit: 100000, // Set a gas limit, how much gas we are prepared to use. Unused gas after execution will be returned to us
@@ -34,11 +33,9 @@ const exchangeETH = async () => {
 
   // Send transactions that have been created from walletSigner
   const trxSend = await walletSigner.sendTransaction(trxBuild);
-  console.log(trxSend);
+  console.log(trxSend); // Check hash value, and check in MetaMask if there is a change in the amount for WETH
   /* Hash value: 
-  0xba9d9f9f62299d9c374d16a6d95692cb0600aa71fbcaa99f0413c689a6b91d83
-  0x225c5500dd24c1fb980b161e6e178b712a16267e89aae57d68829e4ec3b87dd2
-  0x6c4cf9938ac04f878795fd248d49fdc50dbec8ab6d33172dc7fe2bd52598eea8
+  0x2623416348f3f1fc70fdadd6cd2ae2f683063a71369e983aaeea1e525ed63472
   */
 };
 
