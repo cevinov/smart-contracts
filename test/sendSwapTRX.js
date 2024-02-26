3eb76fdb54e765d5e88b7dd3190fb9529feedef1
cevinov <admin@vino0333.my.id>
2024-02-26 16:34:46 +0700

    do validation testing

--- test/sendSwapTRX.js
+++ test/sendSwapTRX.js
@@ -28,4 +28,18 @@ describe("Read and Write to the Blockchain", function () {
 
   // Contract for base token
   contractToken0 = new ethers.Contract(addressFrom, erc20Decimal, provider);
+
+  // DO validation values for each main variable
+  it("Test connection (Provider, Factory, Router, Token)", async function () {
+    assert(provider._isProvider);
+    expect(contractFactory.address).to.equal(
+      "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"
+    );
+    expect(contractRouter.address).to.equal(
+      "0x10ED43C718714eb63d5aA57B78B54704E256024E"
+    );
+    expect(contractToken0.address).to.equal(
+      "0x2170Ed0880ac9A755fd29B2688956BD959F933F8"
+    );
+  });
 });
