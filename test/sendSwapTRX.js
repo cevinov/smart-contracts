1582130178ed577268f9921875b1bdcf0c035a4d
cevinov <admin@vino0333.my.id>
2024-02-27 11:49:34 +0700

    get amount out value

--- test/sendSwapTRX.js
+++ test/sendSwapTRX.js
@@ -16,9 +16,10 @@ describe("Read and Write to the Blockchain", function () {
     contractRouter,
     contractToken0,
     decimals,
-    amountIn,
-    amountOut;
+    amountInSTR,
+    amountOutSTR;
 
+  // Connecting to provider
   const url = "https://bsc-dataseed.bnbchain.org";
   provider = new ethers.providers.JsonRpcProvider(url);
 
@@ -29,17 +30,44 @@ describe("Read and Write to the Blockchain", function () {
   // Contract for base token
   contractToken0 = new ethers.Contract(addressFrom, erc20Decimal, provider);
 
-  // DO validation values for each main variable
-  it("Test connection (Provider, Factory, Router, Token)", async function () {
-    assert(provider._isProvider);
-    expect(contractFactory.address).to.equal(
-      "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"
+  // Get price information
+  const getAmountOut = async function (amountIn) {
+    // Convert amountIn value into blockchain format with decimals
+    const decimals = await contractToken0.decimals();
+    const amountInDec = ethers.utils.parseUnits(amountIn, decimals);
+
+    const amountsOut = await contractRouter.getAmountsOut(amountInDec, [
+      addressFrom,
+      addressTo,
+    ]);
+
+    // Converts the amountOut value to a readable format
+    const amountOutStr = ethers.utils.formatUnits(
+      amountsOut[1].toString(),
+      decimals
     );
-    expect(contractRouter.address).to.equal(
-      "0x10ED43C718714eb63d5aA57B78B54704E256024E"
-    );
-    expect(contractToken0.address).to.equal(
-      "0x2170Ed0880ac9A755fd29B2688956BD959F933F8"
-    );
+    return amountOutStr;
+  };
+
+  //   Check if we got the amountOut value
+  it("Check if we got the amountOut value", async function () {
+    amountInSTR = "5";
+    amountOutSTR = await getAmountOut(amountInSTR);
+    assert(amountOutSTR);
+    console.log("OUT", amountOutSTR);
   });
+
+  // DO validation values for each main variable
+  //   it("Test connection (Provider, Factory, Router, Token)", async function () {
+  //     assert(provider._isProvider);
+  //     expect(contractFactory.address).to.equal(
+  //       "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"
+  //     );
+  //     expect(contractRouter.address).to.equal(
+  //       "0x10ED43C718714eb63d5aA57B78B54704E256024E"
+  //     );
+  //     expect(contractToken0.address).to.equal(
+  //       "0x2170Ed0880ac9A755fd29B2688956BD959F933F8"
+  //     );
+  //   });
 });
