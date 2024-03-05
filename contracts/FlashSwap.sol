ee3f631960df4701ceb768e4808a18ef0967089a
cevinov <admin@vino0333.my.id>
2024-03-05 14:57:40 +0700

    setup smart contract for flashloans

--- contracts/FlashSwap.sol
+++ contracts/FlashSwap.sol
@@ -10,17 +10,10 @@ import "./interfaces/IUniswapV2Pair.sol";
 import "./interfaces/IUniswapV2Router01.sol";
 import "./interfaces/IUniswapV2Router02.sol";
 import "./libraries/UniswapV2Library.sol";
-import "./libraries/SafeMath.sol";
+import "./libraries/SafeERC20.sol";
 
 // Create a contract for a flash loan called FlashSwap
 contract FlashSwap {
-    string private myName = "";
-    function setName(string memory _name) public {
-        myName = _name;
-    }
-
-    function getName() public view returns (string memory) {
-        console.log(myName);
-        return myName;
-    }
+    // SafeERC20 contracts are required for matters that require approval on our behalf
+    using SafeERC20 for IERC20;
 }
