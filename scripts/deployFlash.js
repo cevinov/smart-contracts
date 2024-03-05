8efe4078c482ac33fc57df24140f4da9dca0fcf7
cevinov <admin@vino0333.my.id>
2024-03-05 11:26:20 +0700

    deploy & test smart contract

--- scripts/deployFlash.js
+++ scripts/deployFlash.js
@@ -1,1 +1,22 @@
-console.log("test");
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  // console.log(deployer);
+  console.log("Account for deploying the contract:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Get smart contract for flashloans
+  const Token = await ethers.getContractFactory("FlashSwap");
+  const token = await Token.deploy();
+  await token.deployed();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
