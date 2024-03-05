1816036d459d7b1815cea5f2abeb5db59d3c8761
cevinov <admin@vino0333.my.id>
2024-03-05 17:26:37 +0700

    update

--- contracts/FlashSwap.sol
+++ contracts/FlashSwap.sol
@@ -16,4 +16,14 @@ import "./libraries/SafeERC20.sol";
 contract FlashSwap {
     // SafeERC20 contracts are required for matters that require approval on our behalf
     using SafeERC20 for IERC20;
+
+    // Factory and routing address DEX (PancakeSwap)
+    // https://docs.pancakeswap.finance/developers/smart-contracts/pancakeswap-exchange/v2-contracts
+    address private constant PANCAKE_FACTORY =
+        0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+    address private constant PANCAKE_ROUTER =
+        0x10ED43C718714eb63d5aA57B78B54704E256024E;
+
+    // Menit 3 video 6
+    address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
 }
--- test/flashTest.js
+++ test/flashTest.js
@@ -4,13 +4,14 @@ const { ethers } = require("hardhat");
 
 describe("Test Flashloans Smart Contract", function () {
   it("Check Name", async function () {
+    console.log("test");
     // Get and deploy flashloans smart contract
-    const FlashSwap = await ethers.getContractFactory("FlashSwap");
-    const flashSwap = await FlashSwap.deploy();
-    await flashSwap.deployed();
-    await flashSwap.setName("vino");
+    // const FlashSwap = await ethers.getContractFactory("FlashSwap");
+    // const flashSwap = await FlashSwap.deploy();
+    // await flashSwap.deployed();
+    // await flashSwap.setName("vino");
 
-    // Code block to test the functionality in the smart contract
-    expect(await flashSwap.getName()).to.be.equal("vino");
+    // // Code block to test the functionality in the smart contract
+    // expect(await flashSwap.getName()).to.be.equal("vino");
   });
 });
